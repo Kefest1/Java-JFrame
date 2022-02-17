c738f0b6c4c0c437a61db8ed12c2cb75886d3bd3
Kefest1 <99877119+Kefest1@users.noreply.github.com>
2022-02-17 13:04:41 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,83 @@
+package com.company;
+/*
+import java.awt.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+import java.lang.Math;
+import com.company.Car;
+*/
+/*
+import java.util.Random.*;
+
+import static java.util.Arrays.*;
+
+class Test_1 {
+    private int x;
+    public Test_1(int X){
+        x = X;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public static int mult(int x, int y) {
+        return x * y;
+    }
+
+    public int turbo_mult(int x, int y, int z) {
+        return x * y * z;
+    }
+}
+*/
+/*
+ Border border = BorderFactory.createLineBorder(Color.YELLOW, 3, true);
+
+
+
+        ImageIcon image = new ImageIcon(
+                "C:\\Users\\MASTER\\Desktop\\images.jpg"
+        );
+        JLabel label = new JLabel();
+        label.setText("2137");
+        label.setIcon(image);
+
+        label.setHorizontalTextPosition(JLabel.CENTER);
+        label.setVerticalTextPosition(JLabel.TOP);
+        label.setForeground(Color.yellow);
+        label.setFont(new Font("Mv Boli",Font.PLAIN,18));
+        label.setIconTextGap(-40);
+        label.setBackground(Color.BLACK);
+        label.setOpaque(true);
+        label.setBorder(border);
+        //label.setBounds(0,0,800,800);
+
+        JFrame frame = new JFrame();
+        frame.setSize(850, 850);
+        frame.setTitle("Insert kremówka");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // x = koniec programu
+        frame.getContentPane().setBackground(Color.GRAY);
+
+
+        //frame.setLayout(null);
+        frame.add(label);
+        frame.pack();
+        frame.setVisible(true);
+*/
+import com.company.MyFrame;
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.util.Random;
+
+class Main {
+    public static void main(String[] args) {
+        new MyFrame();
+    }
+}
---
+++ MyFrame.java
@@ -0,0 +1,98 @@
+package com.company;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+public class MyFrame extends JFrame implements ActionListener {
+    ActionListener actionListener1 = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    };
+
+    JButton button1;
+    JButton button2;
+    JButton button3;
+    JButton button4;
+
+    JLabel label;
+
+    MyFrame() {
+        label = new JLabel();
+        label.setText("0");
+        label.setVerticalTextPosition(JLabel.CENTER);
+        label.setHorizontalTextPosition(JLabel.CENTER);
+
+        ImageIcon image = new ImageIcon(
+                "C:\\Users\\MASTER\\Desktop\\istockphoto-496603666-170667a.jpg"
+        );
+
+        //label.setIcon(image);
+        label.setHorizontalTextPosition(JLabel.CENTER);
+        label.setVerticalTextPosition(JLabel.TOP);
+        label.setForeground(Color.yellow);
+        label.setFont(new Font("Mv Boli", Font.PLAIN, 18));
+        label.setIconTextGap(-40);
+        label.setBackground(Color.BLACK);
+        label.setOpaque(true);
+        this.add(label);
+        label.setBounds(450, 450, 100, 100);
+
+        button1 = new JButton();
+        button1.setText("^");
+        button1.setBounds(200, 200, 100, 100);
+
+        button2 = new JButton();
+        button2.setText("v");
+        button2.setBounds(200, 200 + 100, 100, 100);
+
+        button3 = new JButton();
+        button3.setText("<");
+        button3.setBounds(200 - 100, 200 + 100, 100, 100);
+
+        button4 = new JButton();
+        button4.setText(">");
+        button4.setBounds(200 + 100, 200 + 100, 100, 100);
+
+        button1.addActionListener(this);
+        button1.setFocusable(false);
+
+        button2.addActionListener(this);
+        button2.setFocusable(false);
+
+        button3.addActionListener(this);
+        button3.setFocusable(false);
+
+        button4.addActionListener(this);
+        button4.setFocusable(false);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
+        this.setResizable(false);
+        this.setSize(1980, 1080);
+
+
+        this.add(button1);
+        this.add(button2);
+        this.add(button3);
+        this.add(button4);
+
+        this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == button1) {
+            label.setBounds(label.getX(), label.getY() - 25, label.getWidth(), label.getHeight());
+        } else if (e.getSource() == button2) {
+            label.setBounds(label.getX(), label.getY() + 25, label.getWidth(), label.getHeight());;
+        } else if (e.getSource() == button3) {
+            label.setBounds(label.getX() - 25, label.getY(), label.getWidth(), label.getHeight());
+        } else if (e.getSource() == button4) {
+            label.setBounds(label.getX() + 25, label.getY(), label.getWidth(), label.getHeight());
+        }
+    }
+}
